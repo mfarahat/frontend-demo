f9baa22cd0740e0e12e11e4788848a12cb2bf595
Mahmoud Farahat <mahmoudkfarahat@gmail.com>
2020-03-29 23:39:50 -0500

    - added form for customer info     - added date from     - added date to     - added period increment - added customer info component - added material date modules and select modules - added the form with the select and the date components

--- src/app/customer/customer-info/customer-info.component.ts
+++ src/app/customer/customer-info/customer-info.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import * as moment from 'moment';
+import { dateIntervals, DateInterval } from '../../date-interval';
 
 @Component({
   selector: 'app-customer-info',
   templateUrl: './customer-info.component.html',
   styleUrls: ['./customer-info.component.scss']
 })
-export class CustomerInfoComponent implements OnInit {
+export class CustomerInfoComponent {
 
-  constructor() { }
+  form: FormGroup;
+  maxDat: Date;
+  minDate: Date;
+  periodIncOptions: string[];
 
-  ngOnInit(): void {
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      startDate: [''],
+      endDate: [''],
+      periodIncrement: ['']
+    });
+  }
+
+  datesChanged(): void {
+    const start = moment(this.form.controls['startDate'].value);
+    const end = moment(this.form.controls['endDate'].value);
+    const options: string[] = [];
+    dateIntervals.forEach((dateInterval: DateInterval) => {
+      const diff: number = end.diff(start, dateInterval.differenceCategory);
+      if (diff >= dateInterval.differenceCount) {
+        options.push(dateInterval.name);
+      }
+    });
+    this.periodIncOptions = options;
   }
 
 }
--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from "@angular/material/select"
+
+import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { CustomerInfoComponent } from './customer-info/customer-info.component';
-import { CustomerRoutingModule } from './customer-routing.module';
-import { MatTableModule } from '@angular/material/table';
 import { CustomerListService } from './customers-list/customer-list.service';
-
+import { CustomerInfoService } from './customer-info/customer-info.service';
 
 @NgModule({
   declarations: [CustomersListComponent, CustomerInfoComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
     CustomerRoutingModule,
-    MatTableModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule
   ],
-  providers:[CustomerListService]
+  providers: [CustomerListService, CustomerInfoService]
 })
 export class CustomerModule { }
